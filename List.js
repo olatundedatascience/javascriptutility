2f6c298462597a767da8dd2286bb9ed381058e95
Raimi.Aliu <Raimi.Aliu@FCMB-IT-L16582>
2020-06-20 06:23:37 +0100

    added list

---
+++ List.js
@@ -0,0 +1,93 @@
+// list data structures
+
+class List  {
+    #myItem = []
+    constructor(items=[]) {
+        
+        if(items.length > 0) {
+            for(let k of items) {
+                this.#myItem.push(k)
+            }
+        }
+    }
+
+
+    add(item) {
+        this.#myItem.push(item)
+    }
+
+    addAndCheck(item, checkForExistence=false){
+        if(checkForExistence) {
+            let found = this.exist(item)
+            if(!found) {
+                this.add(item)
+            }
+        }
+        else {
+            this.add(item)
+        }
+    }
+
+    get Count() {
+        return this.#myItem.length
+    }
+
+    exist(item) {
+        let found = false;
+        for(var i=0;i<this.#myItem.length;i++) {
+            if(this.#myItem[i] == item) {
+                found = true
+                break;
+            }
+        }
+
+        return found;
+    }
+    insert(index, item){}
+    insert(index, ...items){}
+    update(item){}
+    update(...items){}
+    updateAt(index, item){}
+    remove(item){}
+    indexOf(item){}
+    itemAt(index){}
+    remove(index, item){}
+    removeAt(index){}
+    toString() {
+
+    }
+
+    [Symbol.toString]() {
+
+    }
+
+    [Symbol.iterator]() {
+        let _items = this.#myItem
+
+        return {
+            k :_items.length,
+            _n:0,
+
+            next() {
+                if( this._n <= this.k) {
+                    this._n++
+                    return {done:false, value:_items[this._n]}
+                }
+                else {
+                    return {done:true}
+                }
+            }
+        }
+
+    }
+}
+
+
+//var tl = new List()
+
+var tl = new List()
+tl.add("one")
+tl.add("two")
+tl.addAndCheck("two", true)
+tl.addAndCheck("two", false)
+console.log(tl.Count)
